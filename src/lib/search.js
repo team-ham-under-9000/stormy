70bdc9c3003cef5435cadd2342ebe33c78b28480
holgermanzke <manzkeholger@gmail.com>
2019-11-15 13:25:18 +0100

    connection to api

--- src/lib/search.js
+++ src/lib/search.js
@@ -1,7 +1,8 @@
-// export async function getCity() {
-//   const response = await fetch(
-//     "https://www.metaweather.com/api/location/2487956/"
-//   );
-//   const data = await response.json();
-//   console.log(data);
-// }
+export async function getCity() {
+  const response = await fetch(
+    "https://api.openweathermap.org/data/2.5/weather?lat=37.77&lon=-122.43&appid=b12e52770a4ea6451e533761faa2e8ff"
+  );
+  const data = await response.json();
+
+  console.log(data.wind.speed);
+}
