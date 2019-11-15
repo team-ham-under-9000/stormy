83ef54a6cedd570da1be09bed9d7835e0393385e
holgermanzke <manzkeholger@gmail.com>
2019-11-15 14:14:36 +0100

    add input dynamically to output

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import "babel-polyfill";
 import { getCity } from "./lib/search";
-
 const results = document.querySelector(".results");
 const searchLatt = document.querySelector(".search__latt");
 const searchLong = document.querySelector(".search__long");
 const searchButton = document.querySelector(".search__submit");
 
-getCity().then(data => {
-  const cityName = data.name;
-  results.innerHTML = cityName;
+searchButton.addEventListener("click", () => {
+  const lattValue = searchLatt.value;
+  const longValue = searchLong.value;
+  getCity(lattValue, longValue).then(data => {
+    const cityName = data.name;
+    results.innerHTML = cityName;
+  });
 });
--- src/lib/search.js
+++ src/lib/search.js
@@ -1,6 +1,10 @@
-export async function getCity() {
+export async function getCity(latt, long) {
   const response = await fetch(
-    "https://api.openweathermap.org/data/2.5/weather?lat=37.77&lon=-122.43&appid=b12e52770a4ea6451e533761faa2e8ff"
+    "https://api.openweathermap.org/data/2.5/weather?lat=" +
+      latt +
+      "&lon=" +
+      long +
+      "&appid=b12e52770a4ea6451e533761faa2e8ff"
   );
   const data = await response.json();
   return data;
