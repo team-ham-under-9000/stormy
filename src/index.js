37480d7fdd51dcbd204d020fb608aafe0e817d21
holgermanzke <56833949+holgermanzke@users.noreply.github.com>
2019-11-15 15:52:01 +0100

    Weather (#5)
    * add temperature and div elements in results
    
    * convert kelvin to celcius

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import "babel-polyfill";
 import { getCity } from "./lib/search";
+import { createElements, clearResults } from "./lib/elements";
 const results = document.querySelector(".results");
 const searchLatt = document.querySelector(".search__latt");
 const searchLong = document.querySelector(".search__long");
 const searchButton = document.querySelector(".search__submit");
 
 searchButton.addEventListener("click", () => {
+  clearResults(results);
   const lattValue = searchLatt.value;
   const longValue = searchLong.value;
   getCity(lattValue, longValue).then(data => {
     const cityName = data.name;
-    results.innerHTML = cityName;
+    const cityTemperatureFahrenheit = data.main.temp;
+    const cityTemperature = Math.floor(cityTemperatureFahrenheit - 273.15);
+    const nameResult = createElements("name");
+    const temperatureResult = createElements("temperatur");
+    results.appendChild(nameResult);
+    results.appendChild(temperatureResult);
+    nameResult.innerHTML = cityName;
+    temperatureResult.innerHTML = cityTemperature;
   });
 });
