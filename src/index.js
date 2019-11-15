138dfebf3924c6465e4ffb489dd645dc2b95a4ee
aruppert <56864263+aruppert@users.noreply.github.com>
2019-11-15 16:18:08 +0100

    Weather icon (#7)
    * add weather icon

--- src/index.js
+++ src/index.js
@@ -14,11 +14,16 @@ searchButton.addEventListener("click", () => {
     const cityName = data.name;
     const cityTemperatureFahrenheit = data.main.temp;
     const cityTemperature = Math.floor(cityTemperatureFahrenheit - 273.15);
+    const iconID = data.weather[0].icon;
+    const iconURL = `http://openweathermap.org/img/wn/${iconID}.png`;
     const nameResult = createElements("name");
     const temperatureResult = createElements("temperatur");
+    const iconResult = createElements("icon");
     results.appendChild(nameResult);
     results.appendChild(temperatureResult);
+    results.appendChild(iconResult);
     nameResult.innerHTML = cityName;
     temperatureResult.innerHTML = cityTemperature;
+    iconResult.innerHTML = `<img src="${iconURL}"></img>`;
   });
 });
