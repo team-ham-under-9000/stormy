307abb8c592cb6b7fadb031e704072235cc78d8e
holgermanzke <56833949+holgermanzke@users.noreply.github.com>
2019-11-15 16:53:16 +0100

    add wind information and style results (#8)

--- src/index.js
+++ src/index.js
@@ -12,18 +12,26 @@ searchButton.addEventListener("click", () => {
   const longValue = searchLong.value;
   getCity(lattValue, longValue).then(data => {
     const cityName = data.name;
+    const cityWindSpeed = data.wind.speed;
+    const cityWindDeg = data.wind.deg;
     const cityTemperatureFahrenheit = data.main.temp;
     const cityTemperature = Math.floor(cityTemperatureFahrenheit - 273.15);
     const iconID = data.weather[0].icon;
     const iconURL = `http://openweathermap.org/img/wn/${iconID}.png`;
     const nameResult = createElements("name");
-    const temperatureResult = createElements("temperatur");
+    const temperatureResult = createElements("temperature");
     const iconResult = createElements("icon");
+    const windSpeedResult = createElements("wind-speed");
+    const windDegResult = createElements("wind-deg");
     results.appendChild(nameResult);
     results.appendChild(temperatureResult);
     results.appendChild(iconResult);
+    results.appendChild(windSpeedResult);
+    results.appendChild(windDegResult);
     nameResult.innerHTML = cityName;
     temperatureResult.innerHTML = cityTemperature;
     iconResult.innerHTML = `<img src="${iconURL}"></img>`;
+    windSpeedResult.innerHTML = cityWindSpeed;
+    windDegResult.innerHTML = cityWindDeg;
   });
 });
